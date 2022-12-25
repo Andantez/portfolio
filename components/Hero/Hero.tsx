3a9cd88f31a450cead4e42608a05e10249e7983d
Andantez <kaloyanatanasov83@gmail.com>
2022-12-25 19:53:56 +0000

    feat: add hero, heading and button components basic html structure.

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,18 @@
+import { Button, Heading } from "../ui";
+
+const heading = "I'm Kaloyan, Front-End developer";
+
+const Hero = () => {
+  return (
+    <div>
+      <Heading mainHeading={heading} title="Hello" />
+      <p>
+        Coder, Conceptual thinker with passion for UI effects and animations who
+        loves to build creative things
+      </p>
+      <Button label="My Work" />
+    </div>
+  );
+};
+
+export default Hero;
