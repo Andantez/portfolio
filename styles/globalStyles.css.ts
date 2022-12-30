140d4d31c1282a30ad17e4132c78f8e31447200b
Andantez <kaloyanatanasov83@gmail.com>
2022-12-30 19:08:40 +0000

    clear: merge conflict marker.

--- styles/globalStyles.css.ts
+++ styles/globalStyles.css.ts
@@ -22,32 +22,11 @@ import {
 
 globalStyle(':root', {
   vars: {
-<<<<<<< HEAD
-    [textColor]: "#0D0D0D",
-    [backgroundColor]: "#FFFFFF",
-    [accentColor]: "#FF4250",
-    [titleTextColor]: "#F8F8F8",
-    [placeholderTextColor]: "#DBDBDB",
-    [fwLight]: "300",
-    [fwRegular]: "400",
-    [fwBold]: "700",
-    [fs200]: "0.875rem",
-    [fs300]: "1rem",
-    [fs400]: "1.125rem",
-    [fs500]: "1.5rem",
-    [fs600]: "2rem",
-    [fs650]: "3rem",
-    [fs700]: "4rem",
-    [fs800]: "8rem",
-    [fs900]: "13rem",
-    [gap]: "1.5em",
-=======
     [textColor]: '#0D0D0D',
     [backgroundColor]: '#FFFFFF',
-    [accentColour]: '#FF4250',
-    [lightTextColour]: '#F8F8F8',
-    [darkTextColour]: '#363636',
-    [placeholderTextColour]: '#DBDBDB',
+    [accentColor]: '#FF4250',
+    [titleTextColor]: '#F8F8F8',
+    [placeholderTextColor]: '#DBDBDB',
     [fwLight]: '300',
     [fwRegular]: '400',
     [fwBold]: '700',
@@ -56,37 +35,27 @@ globalStyle(':root', {
     [fs400]: '1.125rem',
     [fs500]: '1.5rem',
     [fs600]: '2rem',
+    [fs650]: '3rem',
     [fs700]: '4rem',
     [fs800]: '8rem',
     [fs900]: '13rem',
     [gap]: '1.5em',
->>>>>>> 40b5e37b7140c77e13fb957d8535f3fe22bbe962
   },
 });
 
 globalStyle('body[data-theme="light"]', {
   vars: {
-<<<<<<< HEAD
-    [textColor]: "#0D0D0D",
-    [backgroundColor]: "#FFFFFF",
-    [titleTextColor]: "#F8F8F8",
-=======
     [textColor]: '#0D0D0D',
     [backgroundColor]: '#FFFFFF',
->>>>>>> 40b5e37b7140c77e13fb957d8535f3fe22bbe962
+    [titleTextColor]: '#F8F8F8',
   },
 });
 
 globalStyle('body[data-theme="dark"]', {
   vars: {
-<<<<<<< HEAD
-    [textColor]: "#FFFFFF",
-    [backgroundColor]: "#0D0D0D",
-    [titleTextColor]: "#363636",
-=======
     [textColor]: '#FFFFFF',
     [backgroundColor]: '#0D0D0D',
->>>>>>> 40b5e37b7140c77e13fb957d8535f3fe22bbe962
+    [titleTextColor]: '#363636',
   },
 });
 
