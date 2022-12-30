04ecf32f9c44b2dc8e0cfb67b23aa43e44c6e6b4
Andantez <kaloyanatanasov83@gmail.com>
2022-12-30 21:31:35 +0000

    feat: add heading component.

--- components/ui/Heading/Heading.tsx
+++ components/ui/Heading/Heading.tsx
@@ -1,12 +1,34 @@
-interface HeadingProps {
-  mainHeading: string;
-  title: string;
-}
-const Heading = ({ mainHeading, title }: HeadingProps) => {
+type Title = 'Projects' | 'About' | 'Contact';
+type AccentText = '.' | '01.' | '02.' | '03.';
+type HeadingProps = {
+  accentType: 'dot' | 'number';
+  accentText: AccentText;
+  headingText: string;
+  title: Title;
+};
+const Heading = ({
+  accentType,
+  accentText,
+  headingText,
+  title,
+}: HeadingProps) => {
+  if (accentType === 'number') {
+    return (
+      <section>
+        <h1>
+          <span>{accentText}</span> {headingText}
+        </h1>
+        <div>{title}</div>
+      </section>
+    );
+  }
   return (
     <section>
-      <h1>{mainHeading}</h1>
-      {/* <div>{title}</div> */}
+      <h1>
+        {headingText}
+        <span>{accentText}</span>
+      </h1>
+      <div>{title}</div>
     </section>
   );
 };
