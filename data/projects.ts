a0e49b1df91e988a88183897b02e35d9aca3bf9b
Andantez <kaloyanatanasov83@gmail.com>
2023-01-03 23:38:29 +0000

    feat: add project images. add image property to project data and its type.

--- data/projects.ts
+++ data/projects.ts
@@ -14,19 +14,21 @@ import {
 } from '../components/icons';
 
 type Stack = {
-  name: string,
-  icon: null |  (() => JSX.Element) 
-}
-type Projects = {
-  name: string,
-  description: string,
-  stack : Stack[]
-}
+  name: string;
+  icon: null | (() => JSX.Element);
+};
+export type Projects = {
+  name: string;
+  description: string;
+  image: string;
+  stack: Stack[];
+};
 const projects: Projects[] = [
   {
     name: 'Meow Portal',
     description:
       'This app gives you extensive information regarding cat breeds and their characteristics to help you choose the right breed for your lifestyle.  You can compare cat breeds based on their characteristics and learn some interesting facts on the way. If you register you can also view the gallery which allows you to browse  and save your favourite pictures for future reference.',
+    image: '/meowPortal.png',
     stack: [
       { name: 'React', icon: ReactJS },
       { name: 'Next.js', icon: NextJS },
@@ -41,6 +43,7 @@ const projects: Projects[] = [
     name: 'Adrastos',
     description: `An online social platform for gamers where people can go to to 
       share their experience or ask for advice. This project was made with my fellow team members as a final project of Le-Wagon boot camp.`,
+    image: '/adrastos.png',
     stack: [
       { name: 'Ruby', icon: Ruby },
       { name: 'Ruby on Rails', icon: Rails },
@@ -53,6 +56,7 @@ const projects: Projects[] = [
     name: 'Tints and Shades',
     description:
       'A tool for your creative projects that helps you generate tints and shades of given colour.',
+    image: '/tintsAndShades.png',
     stack: [
       { name: 'React', icon: ReactJS },
       { name: 'Styled Components', icon: StyledComponents },
